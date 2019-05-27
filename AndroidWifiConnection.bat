e94261aa2d02bcb984b7707819accd763ca08faf
ixyxj <xyxjun@gmail.com>
2019-05-27 17:16:10 +0800

    windows shell

---
+++ AndroidWifiConnection.bat
@@ -0,0 +1,37 @@
+@echo of
+
+adb kill-server
+
+adb devices
+
+adb tcpip 5555
+::adb shell ip addr show wlan0
+
+pause
+@echo of
+adb shell ip route >temp 2>&1
+
+set /p str= <temp
+
+del temp
+
+echo %str%
+
+set n=0
+:her
+call set u=%%str:~%n%,1%%
+if not "%u%"=="" (set /a n+=1
+goto her
+) else (echo length:%n%) 
+
+@echo on
+echo %n%
+
+set /a len=%n%-13
+
+call set u=%%str:~%len%,12%%
+echo %u%:5555
+
+adb connect %u%:5555
+
+pause
